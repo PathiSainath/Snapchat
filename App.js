a3a300dc90020af18fb181ca6c0ff9337ac88433
PathiSainath <130831692+PathiSainath@users.noreply.github.com>
2024-09-12 14:49:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Chatt from './Components/Chatt'
+
+const App = () => {
+  return (
+    <View>
+      <Chatt/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
